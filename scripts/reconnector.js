0679bc4089a8306465e8b391f8554db99f1c53a6
J-Guenther <jannik.guentheg@gmail.com>
2020-11-14 13:48:03 +0100

    players can see button now

--- scripts/reconnector.js
+++ scripts/reconnector.js
@@ -7,9 +7,11 @@ class Reconnector {
 
 Hooks.on("renderSettings", (app, html) => {
     const buttonTitle = game.i18n.localize("Reconnect.ReconnectButton");
-    const reconnectButton = $('<button>' + buttonTitle + '</button>');
+    const reconnectButton = $('<button><i class="fas fa-sync"></i>' + buttonTitle + '</button>');
     html.find("button[data-action='players']").after(reconnectButton);
+    $('#settings-game').find("button[data-action='modules']").after(reconnectButton);
     reconnectButton.click(() => {
+        console.log("Attempt webrtc reconnection");
         Reconnector.reconnect();
     });
 });
